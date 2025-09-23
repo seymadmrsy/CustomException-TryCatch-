7bf672a3650f64236fbb5005ebd8dc1a10827a63
seymadmrsy <seymadmrsy@gmail.com>
2025-09-23 21:50:20 +0300

    personel classı oluşturuldu

---
+++ src/main/java/Personel.java
@@ -0,0 +1,17 @@
+package org.example;
+public class Personel {
+        int id;
+        String ad;
+        String soyad;
+        String pozisyon;
+
+
+
+        void  bilgileriYazdir() {
+
+            System.out.println(id+")" + ad +" " + soyad + " (" + pozisyon + ")");
+
+        }
+
+
+}
